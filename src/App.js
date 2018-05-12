afb7bfce769db5d4b27b9722a362ae586dc68bec
SoinSoin <vincent.retore@gmail.com>
2018-05-12 23:22:36 +0200

    premiere partie presque termié

--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import * as Bootstrap from "react-bootstrap";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      usersGith: []
+      isLoading: true,
+      contacts: []
     };
   }
-
   //je ne suis pas tres sur mais de ce que j'en ai compris c'est ce qui va faire la liaison entre mon composant app et les valeurs de mon fetch que je vais stocké dans l'attreibut contact qui est u n tableau
-  componentDidMount() {
-    this.githubData();
-  }
 
-  //j'utilise la
-  githubData() {
-    fetch("https://api.github.com/users")
+  fetchData() {
+    this.setState({
+      isLoading: true,
+      contacts: []
+    });
+
+    fetch("https://api.github.com/users?since=1")
       .then(response => response.json())
-      .then(githubJson =>githubJson.map(user=>{
-        pseudo: user.login; 
-  }))
-      .then(usersGith =>
+      .then(parsedJSON =>
+        parsedJSON.map(user => ({
+          login: `${user.login}`,
+          id: `${user.id}`,
+          avatar_url: `${user.avatar_url}`
+        }))
+      )
+      .then(contacts =>
         this.setState({
-          usersGith
+          contacts,
+          isLoading: false
         })
       )
-      .catch(console.error("error"));
+      .catch(error => console.log("parsing failed", error));
+  }
+  componentDidMount() {
+    this.fetchData();
   }
   render() {
-    // const { usersGith } = this.state;
+    const { isLoading, contacts } = this.state;
     return (
-      <div className="App">}
+      <div className="App">
+        {!isLoading && contacts.length > 0
+          ? contacts.map(contact => {
+              const { id, avatar_url, login } = contact;
+              return (
+                <Bootstrap.Panel>
+                  <Bootstrap.Panel.Heading>
+                    <Bootstrap.Panel.Title componentClass="h3" />
+                  </Bootstrap.Panel.Heading>
+                  <Bootstrap.Panel.Body key={id}>{login}</Bootstrap.Panel.Body>
+                </Bootstrap.Panel>
+              );
+            })
+          : null}
       </div>
     );
   }
 }
 export default App;
-
-        //  {usersGith.length > 0 ?
-        //    usersGith.map(usersGith => {
-        //       return
-        //       <Bootstrap.Panel>
-        //         <Bootstrap.Panel.Heading>
-        //           <Bootstrap.Panel.Title key={usersGith.name} componentClass="h3">
-        //           {usersGith.name}
-        //           </Bootstrap.Panel.Title>
-        //         </Bootstrap.Panel.Heading>
-        //         <Bootstrap.Panel.Body>Panel content</Bootstrap.Panel.Body>
-        //       </Bootstrap.Panel>;
-        //     })
-        //   : null} 
