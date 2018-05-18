7a953cbeef01bad2f11a5be17972a82c056a69c2
SoinSoin <vincent.retore@gmail.com>
2018-05-18 09:28:35 +0200

    return correction

--- src/components/Teamusers.js
+++ src/components/Teamusers.js
@@ -24,9 +24,9 @@ export class Teamusers extends Component {
 
     //par la  promesse fetch je recupere les données de l'api REST (encore du mal a comprendre le systeme de promesse)
     fetch("https://api.github.com/users?since=0")
-    //cette promesse me permet de recuperer les valeurs JSON de l'api
+      //cette promesse me permet de recuperer les valeurs JSON de l'api
       .then(resp => resp.json())
-    // cette promesse herite de la reponse précédente et me premet de pacourir les reponses avec .map()  ainsi que d'attribuer des clefs à chaque valeur. L'attribut user est la reponse de l'iteration de ma reponse getGitJson
+      // cette promesse herite de la reponse précédente et me premet de pacourir les reponses avec .map()  ainsi que d'attribuer des clefs à chaque valeur. L'attribut user est la reponse de l'iteration de ma reponse getGitJson
       .then(getGitJson =>
         getGitJson.map(user => ({
           login: `${user.login}`,
@@ -41,37 +41,37 @@ export class Teamusers extends Component {
         })
       )
       .catch(error => console.error("error"));
-    }
-    //cette fonction propre a react permet de lié les noeuds entre ma methode constructor et ces propriété et mon composant 
-    componentDidMount() {
-      this.fetchGitData();
-    }
-    //je rend une vue 
-    render() {
-      //je redefinis ma methode usergit en tant que tableau dans le render en lui attribuant  l'etat definis dans le constructor attribut qui contient les resultats de recuperer plus haut. Je declare valeur de users git est dans un tableaux
-    const {usersgit } = this.state;
+  }
+  //cette fonction propre a react permet de lié les noeuds entre ma methode constructor et ces propriété et mon composant 
+  componentDidMount() {
+    this.fetchGitData();
+  }
+  //je rend une vue 
+  render() {
+    //je redefinis ma methode usergit en tant que tableau dans le render en lui attribuant  l'etat definis dans le constructor attribut qui contient les resultats de recuperer plus haut. Je declare valeur de users git est dans un tableaux
+    const { usersgit } = this.state;
     return (
       <div className="Teamusers">
         <Bootstrap.Grid>
           <Bootstrap.Row>
             {/* dans cette partie je vais créer mes composants dynamique */}
-              {
-                // c'est pourquoi ici je parcours usersgit avec .map() et il le parcours que si mon tableau est non null. il me retourn ele resultat dans une vue qui est mon composant. 
-                usersgit.length > 0 ? usersgit.map(usergit => {
-                    return (
-          <Bootstrap.Col key={usergit.id} xs={6} md={2}>
-                  <Bootstrap.Thumbnail align="middle" alt="171x180" src={usergit.avatar_url}>
-                        <h4>{usergit.login}</h4>
-                        <p className="myUser">-{usergit.id}-</p>
-                        <p>
-                          <Bootstrap.Button  data-target={usergit.id} onClick={this.props.recup} bsStyle="success">
-                            View more
+            {
+              // c'est pourquoi ici je parcours usersgit avec .map() et il le parcours que si mon tableau est non null. il me retourn ele resultat dans une vue qui est mon composant. 
+              usersgit.length > 0 ? usersgit.map(usergit => {
+                return (
+                  <Bootstrap.Col key={usergit.id} xs={6} md={2}>
+                    <Bootstrap.Thumbnail align="middle" alt="171x180" src={usergit.avatar_url}>
+                      <h4>{usergit.login}</h4>
+                      <p className="myUser">-{usergit.id}-</p>
+                      <p>
+                        <Bootstrap.Button data-target={usergit.id} onClick={this.props.recup} bsStyle="success">
+                          View more
                           </Bootstrap.Button>
-                        </p>
-                   </Bootstrap.Thumbnail>
-            </Bootstrap.Col>
-                    );
-                  })
+                      </p>
+                    </Bootstrap.Thumbnail>
+                  </Bootstrap.Col>
+                );
+              })
                 : null}
           </Bootstrap.Row>
         </Bootstrap.Grid>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Team, { Teamusers } from "./components/Teamusers";
 import User from "./components/User";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
+
 export default class App extends Component {
 
   constructor(props) {
@@ -15,26 +16,26 @@ export default class App extends Component {
   valTarget() {
     this.setState({
       onRecup: e => {
-        alert(`${e.target.dataset.target}` );
+        alert(`${e.target.dataset.target}`);
       }
     });
-}
+  }
   componentDidMount() {
     this.valTarget();
   }
-  
+
   render() {
     const { onRecup } = this.state;
     return (
-        
-        <Router>
+
+      <Router>
         <div className="App">
           <p>test {this.state.onRecup}</p>
           <Route exact path="/">
             <Teamusers recup={this.state.onRecup} />
           </Route>
           <Route path={"/" + onRecup} component={User} />
-        {console.log(()=>this.state.onRecup())}
+          {console.log(() => this.state.onRecup())}
         </div>
       </Router>
     );
